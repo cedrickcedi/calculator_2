0d76b8db1c91e1ec202f8ba5991f88a5f19ab56a
cedrickcedi <cedrick_cedi@yahoo.com>
2022-03-02 14:26:19 +0800

    commit

--- main.js
+++ main.js
@@ -3,100 +3,107 @@ import './style.css'
 /* constructor enables you to provide any custom initialization that must be done 
 before any other methods can be called on an instantiated object*/
 class Calculator {
-    constructor (calcuComputationText, calcuResultText) {
-        this.calcuComputationText = calcuResultText
-        this.calcuResultText = calcuResultText /* When used in an object method, this refers to the object */
+    constructor (previousOperandText, currentOperandText) {
+      this.previousOperandText = previousOperandText
+      this.currentOperandText = currentOperandText 
         this.clear()
     }
+    /* When used in an object method, 'this' refers to the object */
     
     clear() {
-        this.calcuResult = ''
-        this.calcuComputation = ''
-        this.operation = undefined
+      this.currentOperand = ''
+      this.previousOperand = ''
+      this.operation = undefined
     }
 
     delete() {
-        this.calcuResult = this.calcuResult.toString().slice(0, -1)
+      this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
-        if (number === '.' && this.calcuResult.includes('.')) return /* if number is equal to a period and this string includes a period, return (stop) */
-        this.calcuResult = this.calcuResult.toString() + number.toString() /* toString() returns a number as a string */
+      if (number === '.' && this.currentOperand.includes('.')) return 
+      this.currentOperand = this.currentOperand.toString() + number.toString() 
     }
-    /* && means 'and' (x < 10 && y > 1) is true */
-    /* === equal value and equal type, "x === 5" is true */
+    /* if number is equal to a period and this string includes a period, return (stop) 
+     toString() returns a number as a string 
+     && means 'and' (x < 10 && y > 1) is true 
+     === equal value and equal type, "x === 5" is true */
 
+    /* !== is not equal value or not equal type "x !== 5" false */
     chooseOperation(operation) {
-        if (this.calcuResult === '') return
-        if (this.calcuComputation !== '') { /* !== is not equal value or not equal type "x !== 5" false */
+      if (this.currentOperand === '') return
+      if (this.previousOperand !== '') { 
             this.compute()
         }
         this.operation = operation
-        this.calcuComputation = this.calcuResult
-        this.calcuResult = ''
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
+    /* || means "or" (x == 5 || y == 5) is false */
     compute() {
         let computation
-        const comp = parseFloat(this.calcuComputation)
-        const res = parseFloat(this.calcuResult)
-        if (isNaN(comp) || isNaN(res)) return /* || means "or" (x == 5 || y == 5) is false */
-        switch (this.operation) {
-            case '+':
-                computation = comp + res
-                break
-            case '-':
-                computation = comp - res
-            default:
-                return
-        }
-        this.calcuResult = computation
-        this.operation = undefined
-        this.calcuComputation = ''
+        const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return 
+    switch (this.operation) {
+      case '+':
+        computation = prev + current
+        break
+      case '-':
+        computation = prev - current
+        break
+      default:
+        return
+    }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
     }
     /* parseFloat() method parses a value as a string and returns the first number */
 
     getDisplayNumber(number) {
-        const stringNumber = number.toString()
-        const integerDigits = parseFloat(stringNumber.split('.')[0])
-        const decimalDigits = stringNumber.split('.')[1]
-        let integerDisplay
-        if (isNaN(integerDigits)) {
-          integerDisplay = ''
-        } else {
-          integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
-        }
-        if (decimalDigits != null) {
-          return `${integerDisplay}.${decimalDigits}`
-        } else {
-          return integerDisplay
-        }
+      const stringNumber = number.toString()
+      const integerDigits = parseFloat(stringNumber.split('.')[0])
+      const decimalDigits = stringNumber.split('.')[1]
+      let integerDisplay
+      if (isNaN(integerDigits)) {
+        integerDisplay = ''
+      } else {
+        integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
       }
+      if (decimalDigits != null) {
+        return `${integerDisplay}.${decimalDigits}`
+      } else {
+        return integerDisplay
+      }
+    }
  
     updateDisplay() {
-        this.calcuResult.innerText =
-      this.getDisplayNumber(this.calcuResult)
+      this.currentOperandText.innerText =
+      this.getDisplayNumber(this.currentOperand)
     if (this.operation != null) {
-      this.calcuComputation.innerText =
-        `${this.getDisplayNumber(this.calcuComputation)} ${this.operation}`
+      this.previousOperandText.innerText =
+        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
     } else {
-      this.calcuComputationText.innerText = ''
+      this.previousOperandText.innerText = ''
     }
   }
 }
 
 /* To return all matches (not only the first), use the querySelectorAll() */
-const numberButtons = document.querySelectorAll ('[number]')
-const operationButtons = document.querySelectorAll ('[operation]')
-const equalsButton = document.querySelector ('[equals]')
-const allClearButton = document.querySelector ('[all-clear]')
-const deleteButton = document.querySelector ('[delete]')
-const calcuComputationText = document.querySelector ('[calcu-computation]')
-const calcuResultText = document.querySelector ('[calcu-result]')
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandText = document.querySelector('[data-previous-operand]')
+const currentOperandText = document.querySelector('[data-current-operand]')
 
-const calculator = new Calculator(calcuComputationText, calcuResultText)
+const calculator = new Calculator(previousOperandText, currentOperandText)
 
-numberButtons.forEach(button => {
+/* forEach() method calls a function for each element in an array */
+numberButtons.forEach(button => { 
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
@@ -104,9 +111,10 @@ numberButtons.forEach(button => {
 })
 /* => Arrow functions allow us to write shorter function syntax */
 
+/* innerText Return the text content of an element or node */
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
-        calculator.chooseOperation(button.innerText) /* innerText Return the text content of an element or node */
+        calculator.chooseOperation(button.innerText) 
         calculator.updateDisplay()
     })
 })
